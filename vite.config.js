2c780634a4a099e7b7a67b0e6c7a777314454682
danevski-d <dragan.danevski@ymail.com>
2026-05-11 18:17:37 +0200

    Remove prerender plugin, fix build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import PrerenderPlugin from 'vite-plugin-prerender'
 
 export default defineConfig({
   plugins: [
     react(),
-    PrerenderPlugin({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/'],
-    }),
   ],
   resolve: {
     alias: { '@': path.join(__dirname, 'src') },
